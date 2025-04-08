00ed18e0ff0fb1e3a0f60b2c101a4efa7f5d6e48
Gaziz Malikov <Gazizmalikov0@gmail.com>
2025-04-08 23:52:47 +0500

    remove secret key

--- database/config.py
+++ database/config.py
@@ -6,6 +6,5 @@ load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = f"postgresql+asyncpg://{getenv("DB_USER")}:{getenv("DB_PASS")}@{getenv("DB_HOST")}:{getenv("DB_PORT")}/{getenv("DB_NAME")}"
-    SECRET: str = f"{getenv("SECRET_WORD")}"
     
 settings = Settings()
